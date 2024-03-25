94de95cc1e3df3d35b4616067bcdb1cc9b7123a4
prayagyadav <prayagyadav206@gmail.com>
2024-03-25 09:32:57 -0500

    Data batch script

--- batch_run/Data.sh
+++ batch_run/Data.sh
@@ -16,23 +16,30 @@ mkdir -p ./logs
 #echo "created helper_files.zip"
 
 #DataA
-nohup python runner_Top.py -k DataA -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 1 --end 39 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataA_MET_Run2018_1_to_39.txt 2>&1 &
-echo "DataA_MET_Run2018_1_to_39 submitted with job id $!" | tee -a ./logs/jobids.txt
+nohup python runner_Top.py -k DataA -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 1 --end 40 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataA_MET_Run2018_1_to_40.txt 2>&1 &
+echo "DataA_MET_Run2018_1_to_40 submitted with job id $!" | tee -a ./logs/jobids.txt
 sleep 30
 
 #DataB
-nohup python runner_Top.py -k DataB -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 40 --end 61 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataB_MET_Run2018_40_to_61.txt 2>&1 &
-echo "DataB_MET_Run2018_40_to_61 submitted with job id $!" | tee -a ./logs/jobids.txt
+nohup python runner_Top.py -k DataB -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 1 --end 22 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataB_MET_Run2018_1_to_22.txt 2>&1 &
+echo "DataB_MET_Run2018_1_to_22 submitted with job id $!" | tee -a ./logs/jobids.txt
 sleep 30
 
 #DataC
-nohup python runner_Top.py -k DataC -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 62 --end 85 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataC_MET_Run2018_62_to_85.txt 2>&1 &
-echo "DataC_MET_Run2018_62_to_85 submitted with job id $!" | tee -a ./logs/jobids.txt
+nohup python runner_Top.py -k DataC -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 1 --end 24 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataC_MET_Run2018_1_to_24.txt 2>&1 &
+echo "DataC_MET_Run2018_1_to_24 submitted with job id $!" | tee -a ./logs/jobids.txt
 sleep 30
 
 #DataD
-nohup python runner_Top.py -k DataD -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 86 --end 206 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataD_MET_Run2018_86_to_206.txt 2>&1 &
-echo "DataD_MET_Run2018_86_to_206 submitted with job id $!" | tee -a ./logs/jobids.txt
+nohup python runner_Top.py -k DataD -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 1 --end 50 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataD_MET_Run2018_1_to_50.txt 2>&1 &
+echo "DataD_MET_Run2018_1_to_50 submitted with job id $!" | tee -a ./logs/jobids.txt
 sleep 30
+
+nohup python runner_Top.py -k DataD -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 51 --end 100 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataD_MET_Run2018_51_to_100.txt 2>&1 &
+echo "DataD_MET_Run2018_51_to_100 submitted with job id $!" | tee -a ./logs/jobids.txt
+sleep 30
+
+nohup python runner_Top.py -k DataD -e condor -c 500000 -lepton ${LEPTON} --outdir ${OUTPUT_DIRECTORY} --begin 101 --end 121 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_DataD_MET_Run2018_101_to_121.txt 2>&1 &
+echo "DataD_MET_Run2018_101_to_121 submitted with job id $!" | tee -a ./logs/jobids.txt
 
 echo "The End"
