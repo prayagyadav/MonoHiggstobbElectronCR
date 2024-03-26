e994c2eb89794d9099e019aa4b38733e48c44653
prayagyadav <prayagyadav206@gmail.com>
2024-03-26 10:20:09 -0500

    k

---
+++ batch_run/ttsemi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+echo "CONDOR BATCH SUBMIT"
+echo " "
+echo "MCTTbar1l1v"
+echo "starting..."
+#export COFFEA_IMAGE=coffeateam/coffea-dask-cc7:latest-py3.10
+#export EXTERNAL_BIND=../
+CATEGORY="boosted"
+LEPTON="e"
+WORKERS=8
+REDIRECTOR="fnal"
+OUTPUT_DIRECTORY="debug_Output"
+mkdir -p ./logs
+
+nohup python runner_Top.py -k MCTTbar1l1v -e condor -c 500000 --outdir ${OUTPUT_DIRECTORY} --lepton ${LEPTON} --begin 1 --end 47 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MCTTbar1l1v_1_to_47.txt 2>&1 &
+echo "MCTTbar1l1v_1_to_47 submitted with job id $!" | tee -a ./logs/jobids.txt
+sleep 10
+
+echo "The End"
---
+++ batch_run/wjets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+echo "CONDOR BATCH SUBMIT"
+echo " "
+echo "MCWlvJets"
+echo "starting..."
+#export COFFEA_IMAGE=coffeateam/coffea-dask-cc7:latest-py3.10
+#export EXTERNAL_BIND=../
+CATEGORY="boosted"
+LEPTON="e"
+WORKERS=16
+REDIRECTOR="infn"
+OUTPUT_DIRECTORY="debug_Output"
+mkdir -p ./logs
+
+nohup python runner_Top.py -k MCWlvJets -e condor -c 500000 --outdir ${OUTPUT_DIRECTORY} --lepton ${LEPTON} --begin 1 --end 50 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MCWlvJets_1_to_50.txt 2>&1 &
+echo "MCWlvJets_1_to_50 submitted with job id $!" | tee -a ./logs/jobids.txt
+sleep 30
+
+nohup python runner_Top.py -k MCWlvJets -e condor -c 500000 --outdir ${OUTPUT_DIRECTORY} --lepton ${LEPTON} --begin 51 --end 100 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MCWlvJets_51_to_100.txt 2>&1 &
+echo "MCWlvJets_51_to_100 submitted with job id $!" | tee -a ./logs/jobids.txt
+sleep 30
+
+nohup python runner_Top.py -k MCWlvJets -e condor -c 500000 --outdir ${OUTPUT_DIRECTORY} --lepton ${LEPTON} --begin 101 --end 164 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MCWlvJets_101_to_164.txt 2>&1 &
+echo "MCWlvJets_101_to_164 submitted with job id $!" | tee -a ./logs/jobids.txt
+sleep 30
+
+echo "The End"
